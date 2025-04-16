9bc0133e663630948be89744104244f21e22d25e
Marcello Steiner <steiner.marcello@gmeil.com>
2025-04-16 20:42:44 +0200

    Updated to bulma 1.0.2 and enabled theme switching

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,4 +1,5 @@
-use leptos::{prelude::*, task::spawn_local};
+use leptos::logging::error;
+use leptos::{logging::log, prelude::*, task::spawn_local};
 use leptos_meta::{provide_meta_context, MetaTags, Stylesheet, Title};
 use leptos_router::{
     components::{Route, Router, Routes},
@@ -29,7 +30,7 @@ pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
-        <Stylesheet id="leptos" href="/pkg/cirp.css"/>
+        <Stylesheet id="leptos" href="/pkg/voxlume.css"/>
 
         // sets the document title
         <Title text="Welcome to Leptos"/>
@@ -61,6 +62,33 @@ async fn test_server_fn() -> Result<(), ServerFnError> {
 #[component]
 fn HomePage() -> impl IntoView {
     // Creates a reactive value to update the button
+    // 1. Create a signal to hold the theme state
+    let (theme, set_theme) = signal("light".to_string());
+
+    // 3. Create an effect to update the <html> tag's attribute
+    Effect::new(move |_| {
+        // Get the current theme value from the signal
+        let current_theme = theme.get();
+
+        // Access the document (client-side only)
+        let h: web_sys::Element = document().document_element().expect("boh");
+        match h.set_attribute("data-theme", &current_theme) {
+            Ok(_) => log!("Cool!"),
+            Err(err) => error!("Not cool -> {:?}", err),
+        }
+    });
+
+    // 2. Button to toggle the theme
+    let toggle_theme = move |_| {
+        set_theme.update(|current| {
+            *current = if current == "light" {
+                "dark".to_string()
+            } else {
+                "light".to_string()
+            };
+        });
+    };
+
     let count = RwSignal::new(0);
     let on_click = move |_| *count.write() += 1;
     let server_fn = move |_| {
@@ -72,6 +100,10 @@ fn HomePage() -> impl IntoView {
         <h1>"Welcome to Leptos!"</h1>
         <button on:click=on_click>"Click Me: " {count}</button>
         <br/>
-        <button on:click=server_fn>"Call server fn"</button>
+        <button
+            class="has-text-primary"
+            on:click=server_fn>"Call server fn"</button>
+        <br/>
+        <button on:click=toggle_theme>"Change theme"</button>
     }
 }
