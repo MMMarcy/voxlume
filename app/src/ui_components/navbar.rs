b0d26c70545113b63ebec97b795bd48ef62497e3
Marcello Steiner <steiner.marcello@gmeil.com>
2025-04-20 19:53:36 +0200

    Made the navbar's svg work with a temporary file.

--- app/src/ui_components/navbar.rs
+++ app/src/ui_components/navbar.rs
@@ -1,6 +1,7 @@
 //! Navbar component and utilities to toggle the `is_active` class.
 
 use leptos::{html::HtmlElement, logging, prelude::*};
+use leptos_router::components::A;
 
 #[component]
 pub fn Navbar() -> impl IntoView {
@@ -8,13 +9,13 @@ pub fn Navbar() -> impl IntoView {
     let on_click_handler = move |_| {
         set_is_active.update(|v| *v = !*v);
     };
-    let svg = include_str!("../../../public/test.svg");
+    let svg = include_str!("../../../public/temp.svg");
     view! {
     <nav class="navbar" role="navigation" aria-label="main navigation">
         <div class="navbar-brand">
-            <a class="navbar-item" href="https://bulma.io" inner_html=svg>
-
-            </a>
+            <A class:navbar-item=true href="/">
+                <div inner_html=svg />
+            </A>
 
             <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="top-navbar" on:click=on_click_handler class:is-active=is_active>
             <span aria-hidden="true"></span>
